ed22efbf6fc4b809475c3ee1921be6486a1ab207
tylorspence <tylor.spence@conceptshare.com>
2015-06-16 17:35:40 -0400

    moved some constants

--- app/app.controller.js
+++ app/app.controller.js
@@ -8,16 +8,6 @@
 			'genColorsServiceModule',
 			'menuServiceModule'
 		])
-		.constant('SPEED', {
-			MIN: 1,
-			MAX: 200
-		})
-		.constant('GROWTH_TYPES', {
-			TOGGLER: 1,
-			LIFE_AND_DEATH: 2,
-			NEWTYPE3: 3,
-			NEWTYPE4: 4,
-			NEWTYPE5: 5
-		});
+
 
 })();
--- app/services/menu.service.js
+++ app/services/menu.service.js
@@ -3,7 +3,18 @@
 
 	angular
 		.module('menuServiceModule', [])
-		.service('menuService', menuService);
+		.service('menuService', menuService)
+		.constant('SPEED', {
+			MIN: 1,
+			MAX: 200
+		})
+		.constant('GROWTH_TYPES', {
+			TOGGLER: 1,
+			LIFE_AND_DEATH: 2,
+			NEWTYPE3: 3,
+			NEWTYPE4: 4,
+			NEWTYPE5: 5
+		});
 
 	menuService.$inject = ['GROWTH_TYPES'];
 
