7263b630bfd3ef4e3560e302ea76011be77040c3
tylorspence <tylor.spence@conceptshare.com>
2015-06-20 15:05:27 -0400

    some variable renaming prior to timer refactor

--- app/board/board.directive.js
+++ app/board/board.directive.js
@@ -18,17 +18,17 @@
 	boardController.$inject = ['$scope', '$element', '$window', '$timeout', 'genColors', 'menuService', 'SPEED', 'GROWTH_TYPES', 'LOG_TYPE'];
 	function boardController($scope, $element, $window, $timeout, genColors, menuService, SPEED, GROWTH_TYPES, LOG_TYPE) {
 
-		var ctx = $element[0].getContext('2d');
 		var prom;
 		var w = 0;
 		var h = 0;
 		var cellsW = 0;
 		var cellsH = 0;
-		var automataSquareSize = 15;
-		var automataTimer = 0;
-		var lastXpos = -1;
-		var lastYpos = -1;
-		var automataColors = [];
+		var cellSize = 15;
+		var intTimer = 0;
+		var lastX = -1;
+		var lastY = -1;
+		var gridColorArray = [];
+		var ctx = $element[0].getContext('2d');
 		var surroundingCells = [
 			{y:-1, x:-1},
 			{y:-1, x: 0},
@@ -40,13 +40,6 @@
 			{y: 1, x: 0},
 			{y: 1, x: 1}
 		];
-		var logType = {
-			step: 'STEP',
-			tick: 'TICK',
-			speedChange: 'SPEED CHANGE',
-			clear: 'CLEAR GRID'
-
-		};
 
 		$scope.$on('clearGridEvent', clearGrid);
 		$scope.$on('windowResizeEvent', windowResize);
@@ -54,24 +47,22 @@
 		$scope.$on('drawStepEvent', drawStep);
 		$element.bind('mousedown', mouseDownEvent);
 
-
 		menuService.addLogItem(LOG_TYPE.INIT, 'Welcome to the Log!!!');
-		var automata = newEmptyGrid();
+		var grid = newEmptyGrid();
 		windowResize();
-
 		timer();
 
 		////////////////////////////////////////////////////
 
 		function calculateColorsEvent() {
-			automataColors = genColors.array.hex(menuService.colorOne,menuService.colorTwo,cellsW);
+			gridColorArray = genColors.array.hex(menuService.colorOne,menuService.colorTwo,cellsW);
 		}
 		function clearGrid() {
 			menuService.addLogItem(LOG_TYPE.CLEAR);
-			for (var y = 0; y < automata.length; y++) {
-				for (var x = 0; x < automata[y].length; x++) {
-					automata[y][x].active = false;
-					automata[y][x].count = 0;
+			for (var y = 0; y < grid.length; y++) {
+				for (var x = 0; x < grid[y].length; x++) {
+					grid[y][x].active = false;
+					grid[y][x].count = 0;
 				}
 			}
 		}
@@ -87,16 +78,15 @@
 		function mouseUpEvent(e) {
 			angular.element($window).unbind('mouseup', mouseUpEvent);
 			angular.element($window).unbind('mousemove', mouseMoveEvent);
-			lastXpos = -1;
-			lastYpos = -1;
+			lastX = -1;
+			lastY = -1;
 		}
 
-		var old
 		function windowResize() {
 			w = $window.innerWidth;
 			h = $window.innerHeight;
-			cellsH = Math.ceil(h/automataSquareSize);
-			cellsW = Math.ceil(w/automataSquareSize);
+			cellsH = Math.ceil(h/cellSize);
+			cellsW = Math.ceil(w/cellSize);
 			ctx.canvas.style.width = w + 'px';
 			ctx.canvas.style.height = h + 'px';
 			ctx.canvas.width = w;
@@ -107,25 +97,25 @@
 		}
 
 		function reCalculateGrid() {
-			var height = automata.length > cellsH ? automata.length : cellsH;
-			var width = automata[0].length > cellsW ? automata[0].length : cellsW;
+			var height = grid.length > cellsH ? grid.length : cellsH;
+			var width = grid[0].length > cellsW ? grid[0].length : cellsW;
 			for (var y = -1; y < height+1; y++) {
-				if (angular.isUndefined(automata[y+1])) {
-					automata.push([]);
+				if (angular.isUndefined(grid[y+1])) {
+					grid.push([]);
 				}
 				for (var x = -1; x < width+1; x++) {
-					if (angular.isUndefined(automata[y+1][x+1])) {
-						automata[y+1].push({
-							x: x*automataSquareSize,
-							y: y*automataSquareSize,
+					if (angular.isUndefined(grid[y+1][x+1])) {
+						grid[y+1].push({
+							x: x*cellSize,
+							y: y*cellSize,
 							active: false,
 							count: 0
 						})
 					}
 					//reset cells greater than the grid side
-					else if (x > cellsW-1 || y > cellsH-1) {
-						automata[y+1][x+1].active = false;
-						automata[y+1][x+1].count = 0;
+					if (x > cellsW-1 || y > cellsH-1) {
+						grid[y+1][x+1].active = false;
+						grid[y+1][x+1].count = 0;
 					}
 				}
 			}
@@ -136,8 +126,8 @@
 				var xArray = [];
 				for (var x = -1; x < cellsW+1; x++) {
 					xArray.push({
-						x: x*automataSquareSize,
-						y: y*automataSquareSize,
+						x: x*cellSize,
+						y: y*cellSize,
 						active: false,
 						count: 0}
 					)
@@ -148,13 +138,13 @@
 		}
 
 		function mouseMoveEvent(e) {
-			var x = Math.floor(e.clientX / automataSquareSize) + 1;
-			var y = Math.floor(e.clientY / automataSquareSize) + 1;
-			if((x != lastXpos || y != lastYpos)) {
+			var x = Math.floor(e.clientX / cellSize) + 1;
+			var y = Math.floor(e.clientY / cellSize) + 1;
+			if((x != lastX || y != lastY)) {
 				if (x > 0 && y > 0 && x <= cellsW && y <= cellsH) {
-					lastXpos = x;
-					lastYpos = y;
-					toggle(x,y, automata, true);
+					lastX = x;
+					lastY = y;
+					toggle(x,y, grid, true);
 				}
 			}
 		}
@@ -182,13 +172,13 @@
 
 		function drawGrid() {
 			ctx.clearRect(0,0,w,h);
-			for (var y = 1; y < automata.length-1; y++) {
-				for (var x = 1; x < automata[y].length-1; x++) {
-					var cell = automata[y][x];
+			for (var y = 1; y < grid.length-1; y++) {
+				for (var x = 1; x < grid[y].length-1; x++) {
+					var cell = grid[y][x];
 					if (cell.active) {
 						ctx.beginPath();
-						ctx.fillStyle = automataColors[x];
-						ctx.rect(cell.x,cell.y,automataSquareSize,automataSquareSize);
+						ctx.fillStyle = gridColorArray[x];
+						ctx.rect(cell.x,cell.y,cellSize,cellSize);
 						if (menuService.borders) {
 							ctx.lineWidth = 2;
 							ctx.stroke();
@@ -200,11 +190,11 @@
 			}
 
 			if (menuService.playing) {
-				automataTimer++;
+				intTimer++;
 				var timerSpd = Math.floor(parseInt(SPEED.MAX - menuService.animationSpeed + SPEED.MIN,10)/10) + 1;
-				if (automataTimer % timerSpd == 0) {
+				if (intTimer % timerSpd == 0) {
 					drawStep();
-					menuService.addLogItem(LOG_TYPE.STEP, automataTimer/timerSpd);
+					menuService.addLogItem(LOG_TYPE.STEP, intTimer/timerSpd);
 				}
 			}
 		}
@@ -212,8 +202,8 @@
 			var newGrid = newEmptyGrid();
 			for (var y = 1; y < newGrid.length-1; y++) {
 				for (var x = 1; x < newGrid[y].length-1; x++) {
-					var count = automata[y][x].count;
-					if (automata[y][x].active) {
+					var count = grid[y][x].count;
+					if (grid[y][x].active) {
 						if (newGrid[y][x].active !== menuService.activeGrowthType.stayAlive.indexOf(count) > -1) {
 							toggle(x,y,newGrid);
 						}
@@ -223,13 +213,13 @@
 					}
 				}
 			}
-			automata = angular.copy(newGrid);
+			grid = angular.copy(newGrid);
 		}
 		function growthAnimation1() {
-			var newGrid = angular.copy(automata);
+			var newGrid = angular.copy(grid);
 			for (var y = 1; y < newGrid.length-1; y++) {
 				for (var x = 1; x < newGrid[y].length-1; x++) {
-					if (automata[y][x].active) {
+					if (grid[y][x].active) {
 						toggle(x,y, newGrid);
 						for (var i = 0; i < menuService.activeGrowthType.activatedCells.length; i++) {
 							var index = menuService.activeGrowthType.activatedCells[i] - 1;
@@ -242,7 +232,7 @@
 					}
 				}
 			}
-			automata = angular.copy(newGrid);
+			grid = angular.copy(newGrid);
 		}
 
 	}
